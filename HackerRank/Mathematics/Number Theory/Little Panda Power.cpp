3b7621a8295464ddd7ca23a6302eed14411e83aa
zcorel <zcorel@hotmail.com>
2023-01-02 13:18:30 +0800

    a

--- HackerRank/Mathematics/Number Theory/Little Panda Power.cpp
+++ HackerRank/Mathematics/Number Theory/Little Panda Power.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 typedef vector<int> vi;
 
+// brute
 int solve(int a, int b, int x) {
     if (x==1) {
         return 0;
@@ -18,10 +19,10 @@ int solve(int a, int b, int x) {
     int ax = a%x;
 
     vi re;
-    long l = 1;
-    int i = 0;
+    long l=1L;
+    int i=0;
     for (; i<m; ++i) {
-        l = (l*ax)%x;
+        l=(l*ax)%x;
         re.push_back(l);
         if (l==1) {
             break;
@@ -37,6 +38,38 @@ int solve(int a, int b, int x) {
     }
 }
 
+// a*b%m=1
+void extended_euclidean(int a, int b, int &x, int &y) {
+    if (b==0) { x=1; y=0; return; }
+    extended_euclidean(b, a%b, y, x);
+    y -= a/b*x;
+}
+
+int power(int x, int y, int m) {
+    if (m==1) {
+        return 0;
+    }
+
+    if (y==0) return 1;
+
+    if (y<0) {
+        y=-y;
+
+        int z;
+        extended_euclidean(x, m, x, z);
+        if (x<0) x+=m;
+    }
+
+    if (y==1) return x;
+    
+    long pp = power(x, y>>1, m);
+    long ret = (pp*pp)%m;
+    if (y&1) {
+        ret = (ret*x)%m;
+    }
+    return ret;
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
@@ -46,7 +79,7 @@ int main() {
     cin >> t;
     while (t--) {
         cin >> a >> b >> x;
-        cout << solve(a, b, x) << "\n";
+        cout << power(a, b, x) << "\n";
     }
 
     // cout << "Time = " << double(clock()) / CLOCKS_PER_SEC << "\n";
